99a94449c8822ab24deb15d057cd68770aa612dd
MattGThomas <matthewgthomas0616@gmail.com>
2019-04-23 18:04:33 -0500

    answered questions 1 and 2 prototypes

--- assignments/this.js
+++ assignments/this.js
@@ -1,8 +1,9 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
+* 1. Window binding is the default behavior for the "this" keyword in javascript
+* 2. Implicit binding happens when, dot notation is used to invoke a function, the left of the dot becomes the context for 
+'this'
 * 3. 
 * 4. 
 *
@@ -12,10 +13,23 @@
 // Principle 1
 
 // code example for Window Binding
+function windowExample(example) {
+    console.log(this);
+    return `I am an ${example} of window binding in js.`
+}
 
 // Principle 2
+ 
+//code example for Implicit Binding
+const dog = {
+    name: 'fido',
+    age: 3,
+    speek: function() {
+        return `woof woof`
+    }
+}
+console.log(dog.speek())
 
-// code example for Implicit Binding
 
 // Principle 3
 
